1346e36dbc185cd5eed782e3745f1f2c2fd3d445
Björn <29443@ma-web.nl>
2019-09-20 10:05:32 +0200

    For the bronze!

---
+++ dotnet/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Hello World!");
+
+            //Vraag om dick picss
+            Console.ReadLine();
+        }
+    }
+}
